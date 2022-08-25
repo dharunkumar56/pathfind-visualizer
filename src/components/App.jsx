17aa80a64747fa7d50f165a4f8153a00a5318321
dharunkumar56 <dharunkumaroff@gmail.com>
2022-08-25 19:30:59 +0530

    commit message

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
 import React, {useState, useEffect} from "react";
 import Grid from "./Grid";
 import BFS from "../algorithms/BFS";
-import BFSVisited from "../algorithms/BFSVisited";
 import DFS, {setInitial, getVisited, getPath} from "../algorithms/DFS";
 import Astar, {setInitialFromAstar, getPathFromAstar , getVisitedFromAstar} from "../algorithms/Astar";
-import { hasSelectionSupport } from "@testing-library/user-event/dist/utils";
-
 var mainGrid = [];
 var rows = 30;
 var cols = 70;
-var done = true;
-var pathFound = false;
 
 //15 25
 //15 45
@@ -98,7 +93,7 @@ function App(){
             }
             }
             document.getElementById(`node-${15}-${10}`).classList.add("start");
-            document.getElementById(`node-${15}-${55}`).classList.add("start");
+            document.getElementById(`node-${15}-${55}`).classList.add("end");
         }
         else{
             alert("Wait for the visualization to complete");
@@ -120,9 +115,6 @@ function App(){
             alert("no path Found");
             handleClear();
             return;
-        }
-        else{
-            pathFound = true;
         }
         for(let i = 0; i < visited.length; i++){
             if(i === visited.length - 1){
@@ -296,18 +288,40 @@ function App(){
                 
             </div>
             <div className="row">
-                <div className="col-lg-6">
+                <div className="col-lg-4">
                 <button onClick={handleClear} className="btn btn-primary">Clear Board</button>
                 <button onClick={handleBFS} className="btn btn-primary">BFS</button>
                 <button onClick={handleDFS} className="btn btn-primary">DFS</button>
                 <button onClick={handleAstar} className="btn btn-primary">A* search</button>
-                <button onClick={handleDijkstras} className="btn btn-primary">Dijkstra's</button>
+                <button onClick={handleDijkstras} className="btn btn-primary">Dijkstra's Algorithm</button>
                 <button onClick={handleRandomBlocks} className="btn btn-primary">Generate Random Blocks</button> 
                 </div>
-                <div className="col-lg-6">
+                <div className="col-lg-4">
                     <p>Step1: Clear board and Press generate random blocks or click on a node to make it a block</p>
-                    <p>Step2: Select either BFS or DFS path finding algorithm</p>
+                    <p>Step2: Select a path finding algorithm</p>
                     <p>Step3: After the algorithm completes execution, press "Clear Board"</p>
+                </div>
+                <div className="col-lg-4">
+                    <p className="new">Start and end node: &nbsp; &nbsp;</p>
+
+                    <div className="example-start new">
+
+                    </div>
+                    <br></br>
+                    <p className="new">Visited Node: &nbsp; &nbsp;</p>
+                    <div className="visited new">
+
+                    </div>
+                    <br></br>
+                    <p className="new">Node in Path: &nbsp; &nbsp;</p>
+                    <div className="example-path new">
+
+                    </div>
+                    <br></br>
+                    <p className="new">Block Nodes: &nbsp; &nbsp;</p>
+                    <div className="example-wall new">
+
+                    </div>
                 </div>
             </div>
             <div id="board">
